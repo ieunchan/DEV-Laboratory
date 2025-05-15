0fb01abf590a4ad49cfda2ba7d24a14d9f3ac7e5
이은찬 <jsmisth26@gmail.com>
2025-05-15 17:31:07 +0900

    coding test

---
+++ coding_test/추억 접기.py
@@ -0,0 +1,24 @@
+# 사진 속에 나오는 인물의 그리움 점수를 모두 합산한 값이 해당 사진의 추억 점수가 됩니다
+# 예를 들어 사진 속 인물의 이름이 ["may", "kein", "kain"]이고 각 인물의 그리움 점수가 [5점, 10점, 1점]일 때 해당 사진의 추억 점수는 16(5 + 10 + 1)점이 됩니다
+# 
+
+
+
+def solution(name, yearning, photo):
+    stack = []
+    score = dict(zip(name, yearning))
+
+    for i in photo:
+        total = 0
+        for person in i:
+            total += score.get(person,0)
+        stack.append(total)
+    return stack
+
+name = ["may", "kein", "kain", "radi"]
+yearning = [5, 10, 1, 3]
+photo = [["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]]
+
+
+result = solution(name, yearning, photo)
+print(result)
