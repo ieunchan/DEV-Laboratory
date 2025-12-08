fd334a3d854bcaed8ceb980c90d8afb09eddc19b
이은찬 <jsmisth26@gmail.com>
2025-12-08 11:57:13 +0900

    codingtest

--- coding_test/문자열마음대로정렬하기.py
+++
@@ -1,8 +0,0 @@
-#  각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬
-
-
-def solution(strings, n):
-    return sorted(strings, key=lambda x: (x[n], x))
-
-print(solution(["sun", "bed", "car"], 1))
-print(solution(["abce", "abcd", "cdx"],2))
